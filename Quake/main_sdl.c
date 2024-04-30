708e9cce0979ed36304d025eddd813a1cba36a72
drHyperion451 <drHyperion451@proton.me>
2024-04-30 19:23:23 +0100

    MacOS successfully compiles!

--- Quake/main_sdl.c
+++ Quake/main_sdl.c
@@ -118,11 +118,7 @@ static double Sys_Throttle (double oldtime)
 
 static quakeparms_t	parms;
 
-// On OS X we call SDL_main from the launcher, but SDL2 doesn't redefine main
-// as SDL_main on OS X anymore, so we do it ourselves.
-#if defined(__APPLE__)
-#define main SDL_main
-#endif
+
 
 int main(int argc, char *argv[])
 {
